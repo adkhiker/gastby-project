07738ec05806b422bf68914883cfa8423f4507d1
adkhiker <poirier.greg@gmail.com>
2019-07-21 07:17:52 -0400

    setup master

---
+++ .cache/async-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("C:\\Users\\poiri\\Desktop\\React\\hello-world\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("C:\\Users\\poiri\\Desktop\\React\\hello-world\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
